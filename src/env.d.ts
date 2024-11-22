33a5c4e35017db1b6576090e5039d05666c99ce6
jgpark-1 <jgpark7281@gmail.com>
2024-11-22 15:51:10 +0900

    chore: pdf, theme 관련 환경 변수 추가

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly PDF_VIEW: string;
+  readonly FORCE_THEME: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
